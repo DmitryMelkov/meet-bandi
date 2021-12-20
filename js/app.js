960979ba2a2c5350f4ec5623291df1ebaeddb6bd
Дмитрий <92420054+DmitryMelkov@users.noreply.github.com>
2021-12-20 15:31:36 +0500

    add corrections

--- js/app.js
+++ js/app.js
@@ -12,6 +12,32 @@ window.addEventListener("DOMContentLoaded", function () {
     burger.classList.toggle("active");
   }
 
+    //прокрутка при клике
+    const menuLinks = document.querySelectorAll(".header__nav-link[data-goto]");
+
+    if (menuLinks.length > 0) {
+      menuLinks.forEach((menuLink) => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+      });
+
+      function onMenuLinkClick(e) {
+        const menuLink = e.target;
+
+        //проверка есть ли такой атрибут
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+          const gotoBlock = document.querySelector(menuLink.dataset.goto);
+          const gotoBlockValue =
+            gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector("header").offsetHeight;
+
+          window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth",
+          });
+          e.preventDefault();
+        }
+      }
+    }
+
   //яндекс карта
   // Функция ymaps.ready() будет вызвана, когда
   // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
